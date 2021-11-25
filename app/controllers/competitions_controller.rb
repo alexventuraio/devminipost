841a26266b2301d579aafa583841377dd08e3d30
Alejandro Ventura Contreras <soyalexventura@gmail.com>
2021-11-24 18:54:47 -0600

    Show prizes records for each competition on the index page.

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -4,7 +4,7 @@ class CompetitionsController < ApplicationController
 
   # GET /competitions or /competitions.json
   def index
-    @competitions = Competition.all
+    @competitions = Competition.includes(:prizes)
   end
 
   # GET /competitions/1 or /competitions/1.json
