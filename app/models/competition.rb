7a7a76c9fdce5b381778e985b699025563903521
Alejandro Ventura Contreras <soyalexventura@gmail.com>
2021-12-01 08:59:13 -0600

    Show the total of Prizes value in the Competition.

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -2,5 +2,5 @@ class Competition < ApplicationRecord
   belongs_to :user
   has_many :prizes, dependent: :destroy
 
-  accepts_nested_attributes_for :prizes, reject_if: :all_blank
+  accepts_nested_attributes_for :prizes, reject_if: :all_blank, allow_destroy: true
 end
